a76d216d6774a5bcfc1fb70e58b02fa2d3914548
Carlos Requena López <crequena@ulb.ac.be>
2018-11-20 19:29:53 +0100

    switch extract-text-webpack-plugin for mini-css plugin

--- webpack.prod.js
+++ webpack.prod.js
@@ -2,7 +2,7 @@ const path = require('path');
 const webpack = require('webpack');
 const merge = require('webpack-merge');
 const UglifyJSPlugin = require('uglifyjs-webpack-plugin');
-const ExtractTextPlugin = require("extract-text-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 const common = require('./webpack.common.js');
 
 module.exports = merge(common, {
@@ -10,32 +10,36 @@ module.exports = merge(common, {
     rules: [
       {
         test: /\.s?css$/,
-        use: ExtractTextPlugin.extract({
-          use: [
-            {
-              loader: "css-loader", // translates CSS into CommonJS
-            },
-            {
-              loader: "sass-loader", // compiles Sass to CSS
-              options: {/* also use "~" in prefix for node_modules stylesheets */
-                includePaths: [path.resolve(__dirname, "styles")],
-                implementation: require("dart-sass")
-              }
-            }]
-        })
+        use: [
+          {
+            loader: MiniCssExtractPlugin.loader,
+          },
+          {
+            loader: "css-loader", // translates CSS into CommonJS
+          },
+          {
+            loader: "sass-loader", // compiles Sass to CSS
+            options: {/* also use "~" in prefix for node_modules stylesheets */
+              includePaths: [path.resolve(__dirname, "styles")],
+              implementation: require("dart-sass")
+            }
+          }]
       }]
   },
   plugins: [
-    new ExtractTextPlugin({
-      filename: "[contenthash]-main.css",
+    new MiniCssExtractPlugin({
+      // Options similar to the same options in webpackOptions.output
+      // both options are optional
+      filename: "[name].css",
+      chunkFilename: "[id].css"
     }),
     new UglifyJSPlugin({
       parallel: 4
     }),
     new webpack.DefinePlugin({
       /* Important ! In prod, there is no need for a specific
-      port. This value should be http://localhost:3000 in dev to take
-      advantage of the auto-reloading webpack server */
+         port. This value should be http://localhost:3000 in dev to take
+         advantage of the auto-reloading webpack server */
       API_URL: JSON.stringify(""),
     })
 
